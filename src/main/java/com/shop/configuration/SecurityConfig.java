717ae972acbea89b1800afd14fba20e5272b1f67
SerhiiMischenko <serj.roshen@gmail.com>
2023-07-27 17:38:07 +0300

    SecurityConfig.java fix .authorizeRequests(), fix product-info.ftlh

--- src/main/java/com/shop/configuration/SecurityConfig.java
+++ src/main/java/com/shop/configuration/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers( "/product/**", "/images/**", "/registration")
+                .antMatchers("/" , "/product/**", "/images/**", "/registration")
                 .permitAll()
                 .anyRequest().authenticated()
                 .and()
